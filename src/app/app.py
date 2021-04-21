88b3594f9188976502b0916a8e2c01904da816c2
Gabrielle Curcio <33849234+Gabscurioshop@users.noreply.github.com>
2021-04-21 16:55:44 -0400

    added homepage,log-in and log-out templates

--- src/app/app.py
+++ src/app/app.py
@@ -51,7 +51,8 @@ https://www.geeksforgeeks.org/python-using-for-loop-in-flask/
 
 import psycopg2
 from config import config
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
  
 def connect(query):
     """ Connect to the PostgreSQL database server """
@@ -85,20 +86,60 @@ def connect(query):
  
 # app.py
 
+#create application object
 app = Flask(__name__)
 
+app.secret_key = "My precious"
 
-# serve form web page
+#login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('login'))
+    return wrap
+  
+#use decorators to link functino to url
 @app.route("/")
-def form():
-    return render_template('my-form.html')
+@login_required
+def home():
+    #return "Welcome to Trentaudio!" #return string
+    return render_template('index.html') #render template
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html') #render template
+ 
+#handle login requests
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again.'
+        else:
+            session['logged_in'] =True
+            flash('You were just logged in!')
+            return redirect(url_for('home'))
+    return render_template('login.html', error = error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out!')
+    return redirect(url_for('welcome'))
+
+#def form():
+ #   return render_template('my-form.html')
 
 # handle form data
-@app.route('/form-handler', methods=['POST'])
-def handle_data():
-    rows = connect(request.form['query'])
+#@app.route('/form-handler', methods=['POST'])
+#def handle_data():
+ #   rows = connect(request.form['query'])
 
-    return render_template('my-result.html', rows=rows)
+   # return render_template('my-result.html', rows=rows)
 
 if __name__ == '__main__':
     app.run(debug = True)
